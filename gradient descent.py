bb668c504ec0c8de8f2a8e0910641178beb5b6b6
luvMint <lovejd0815@gmail.com>
2022-06-02 03:18:18 +0900

    경사하강법 코드 분석

--- gradient descent.py
+++ gradient descent.py
@@ -21,25 +21,25 @@ def plot_prediction(pred, y):
     plt.show
 
 ## Gradient Descent 구현 구간
-W = np.random.uniform(-1, 1) 
-b = np.random.uniform(-1, 1)
+W = np.random.uniform(-1, 1) #np.random.uniform()은 균등분포 함수이다. 최소값, 최대값, 데이터 개수 순서로 파라미터를 입력해줌. 여기에는 1과 -1뿐이라 그 사이의 값을 뽑아준다.
+b = np.random.uniform(-1, 1) #가중치와 바이어스를 -1과 1사이의 임의의 값으로 초기화.
 
 learning_rate = 0.7
 
-for epoch in range(100):
+for epoch in range(100): #epoch는 학습진행을 의미
     Y_Pred = W * X + b
 
-    error = np.abs(Y_Pred - Y).mean()
-    if error < 0.001:
-        break
+    error = np.abs(Y_Pred - Y).mean() #평균 오차 정의, abs: 숫자의 절대값을 구하는 데에 사용되는 함수
+    if error < 0.001: 
+        break #error의 값이 0.001미만이 될 때 까지 계속 돌림.
     #gradient descent 
     w_grad = learning_rate * ((Y_Pred - Y)*X).mean()
     b_grad = learning_rate * ((Y_Pred - Y)).mean()
 
-    #W, b값 갱신 
+    #W, b값 갱신 (error의 값이 0.001이 될 때 까지)
     W = W - w_grad
     b = b - b_grad
 
     if epoch % 5 == 0:
         Y_Pred = W * X + b
-        plot_prediction(Y_Pred, Y)
+        plot_prediction(Y_Pred, Y) #plot_prediction 함수 이용
